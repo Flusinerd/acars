3658e897be9a2986f25a66fe70bfebdad0b1e884
Jan Krueger <jankrueger1999@gmail.com>
2021-01-31 21:43:04 +0100

    fix(flightNo-validator): Add MQC as valid flight number

--- src/app/shared/validators/flightNo.validator.ts
+++ src/app/shared/validators/flightNo.validator.ts
@@ -3,7 +3,7 @@ import { AbstractControl } from '@angular/forms';
 export function flightNoValidator() {
   return (control: AbstractControl): {[key: string]: any} | null => {
     const value = control.value as string;
-    const containsMQT = value.includes('MQT');
+    const containsMQT = value.includes('MQT') || value.includes('MQC');
     return containsMQT ? null : { callsign: { value }};
   };
 }
